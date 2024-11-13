a151dbc0a6f508de70763e76b978560b633bf020
Luke <129730838+LukeNinjaX@users.noreply.github.com>
2024-11-13 16:25:08 +0800

    fix(#6): code issue of keys add recover (#7)

--- app/codec.go
+++ app/codec.go
@@ -1,7 +1,9 @@
 package app
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
 
 	crypto "github.com/artela-network/artela-rollkit/ethereum/crypto/codec"
 	"github.com/artela-network/artela-rollkit/ethereum/types"
@@ -11,4 +13,11 @@ import (
 func RegisterInterfaces(interfaceRegistry codectypes.InterfaceRegistry) {
 	crypto.RegisterInterfaces(interfaceRegistry)
 	types.RegisterInterfaces(interfaceRegistry)
+}
+
+// RegisterLegacyAminoCodec registers Interfaces from types, crypto, and SDK std.
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	sdktypes.RegisterLegacyAminoCodec(cdc)
+	crypto.RegisterCrypto(cdc)
+	codec.RegisterEvidences(cdc)
 }
---
+++ app/encoding.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	amino "github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+
+	"github.com/artela-network/artela-rollkit/app/params"
+)
+
+// MakeConfig creates an EncodingConfig for testing
+func MakeConfig(mb module.BasicManager) params.EncodingConfig {
+	cdc := amino.NewLegacyAmino()
+	interfaceRegistry := types.NewInterfaceRegistry()
+	codec := amino.NewProtoCodec(interfaceRegistry)
+
+	encodingConfig := params.EncodingConfig{
+		InterfaceRegistry: interfaceRegistry,
+		Marshaler:         codec,
+		TxConfig:          tx.NewTxConfig(codec, tx.DefaultSignModes),
+		Amino:             cdc,
+	}
+
+	RegisterLegacyAminoCodec(encodingConfig.Amino)
+	mb.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	mb.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	return encodingConfig
+}

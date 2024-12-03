4312d8191412f76c434018431557f4634f97565a
Luke <eric@artela.network>
2024-12-03 11:16:51 +0800

    fix(#9): add UnpackInterfaces for trace tx proto message

---
+++ x/evm/types/query_unpacker.go
@@ -0,0 +1,28 @@
+package types
+
+import (
+	codec "github.com/cosmos/cosmos-sdk/codec/types"
+)
+
+var (
+	_ codec.UnpackInterfacesMessage = (*QueryTraceTxRequest)(nil)
+	_ codec.UnpackInterfacesMessage = (*QueryTraceBlockRequest)(nil)
+)
+
+func (m QueryTraceTxRequest) UnpackInterfaces(unPacker codec.AnyUnpacker) error {
+	for _, msg := range m.Predecessors {
+		if err := msg.UnpackInterfaces(unPacker); err != nil {
+			return err
+		}
+	}
+	return m.Msg.UnpackInterfaces(unPacker)
+}
+
+func (m QueryTraceBlockRequest) UnpackInterfaces(unPacker codec.AnyUnpacker) error {
+	for _, msg := range m.Txs {
+		if err := msg.UnpackInterfaces(unPacker); err != nil {
+			return err
+		}
+	}
+	return nil
+}
